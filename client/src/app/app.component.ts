b11276dbc8195adebf8581cc6b12acfe9f69dd35
YaroslavHorlach <horlachyaroslav@gmail.com>
2023-09-19 14:41:22 +0300

    End of section 9

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,7 +1,4 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { IProduct } from './models/product';
-import { IPagination } from './models/pagination';
 
 @Component({
     selector: 'app-root',
@@ -10,15 +7,9 @@ import { IPagination } from './models/pagination';
 })
 export class AppComponent implements OnInit {
     title = 'W&S-Store';
-    products: IProduct[];
 
-    constructor(private http: HttpClient) {}
+    constructor() {}
 
     ngOnInit() {
-        this.http.get('https://localhost:7151/api/products?pageSize=50').subscribe((response: IPagination) => {
-            this.products = response.data;
-        }, error => {
-            console.log(error);
-        });
     }
 }
