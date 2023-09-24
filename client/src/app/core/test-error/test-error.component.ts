b7c17c391c5f990d5a2d4b61220e70c5d403deb9
YaroslavHorlach <horlachyaroslav@gmail.com>
2023-09-24 21:43:32 +0300

    End of section 11

---
+++ client/src/app/core/test-error/test-error.component.ts
@@ -0,0 +1,47 @@
+import { HttpClient } from '@angular/common/http';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-test-error',
+  templateUrl: './test-error.component.html',
+  styleUrls: ['./test-error.component.scss']
+})
+export class TestErrorComponent {
+    baseUrl = 'https://localhost:7151/api/';
+    validationErrors: any;
+
+    constructor(private http: HttpClient) {}
+
+    get404Error() {
+        this.http.get(this.baseUrl + 'products/42').subscribe(response => {
+            console.log(response);
+        }, error => {
+            console.log(error);
+        });
+    }
+
+    get500Error() {
+        this.http.get(this.baseUrl + 'buggy/servererror').subscribe(response => {
+            console.log(response);
+        }, error => {
+            console.log(error);
+        });
+    }
+
+    get400Error() {
+        this.http.get(this.baseUrl + 'buggy/badrequest').subscribe(response => {
+            console.log(response);
+        }, error => {
+            console.log(error);
+        });
+    }
+
+    get400ValidationError() {
+        this.http.get(this.baseUrl + 'products/fourtytwo').subscribe(response => {
+            console.log(response);
+        }, error => {
+            console.log(error);
+            this.validationErrors = error.errors;
+        });
+    }
+}
